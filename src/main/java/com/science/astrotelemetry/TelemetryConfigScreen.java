ba80a5401617c4fd7afd15c17627197f0095e547
artem26841 <artem.andrey.bulat@gmail.com>
2026-08-08 18:21:55 +0300

    TelemetryConfigScreen.java

--- src/main/java/com/science/astrotelemetry/TelemetryConfigScreen.java
+++ src/main/java/com/science/astrotelemetry/TelemetryConfigScreen.java
@@ -18,10 +18,11 @@ public class TelemetryConfigScreen extends Screen {
 
     @Override
     protected void init() {
+        if (this.minecraft == null || this.minecraft.player == null) return;
+
         int pX = this.width / 2;
         int pY = this.height / 2;
 
-        // Поля ввода данных
         this.nameField = new EditBox(this.font, pX - 100, pY - 60, 200, 20, Component.literal("Имя"));
         this.nameField.setValue("Новый Сектор");
         this.addWidget(this.nameField);
@@ -35,41 +36,42 @@ public class TelemetryConfigScreen extends Screen {
         this.addWidget(this.zField);
 
         this.radiusField = new EditBox(this.font, pX - 100, pY - 10, 200, 20, Component.literal("Радиус"));
-        this.radiusField.setValue("6"); // По умолчанию ставим 6 блоков, как вы просили!
+        this.radiusField.setValue("6"); 
         this.addWidget(this.radiusField);
 
-        // Кнопка сохранения зоны
-        this.addRenderableWidget(Button.builder(Component.literal("Добавить Зону"), (button) -> {
+        // Обновленный билдер кнопок под Forge 47.4.22
+        Button saveButton = Button.builder(Component.literal("Добавить Зону"), (button) -> {
             try {
                 String name = this.nameField.getValue();
                 double x = Double.parseDouble(this.xField.getValue());
                 double z = Double.parseDouble(this.zField.getValue());
                 double radius = Double.parseDouble(this.radiusField.getValue());
 
-                // Создаем зону и автоматически пишем её в JSON
                 ZoneManager.addZone(new TelemetryZone(name, x, z, radius, 1420.4, "Спутник"));
-                this.onClose(); // Закрываем меню
+                this.onClose(); 
             } catch (NumberFormatException e) {
-                // Если ввели буквы вместо цифр
                 this.nameField.setValue("ОШИБКА ВВОДА!");
             }
-        }).bounds(pX - 100, pY + 20, 200, 20).build());
+        }).bounds(pX - 100, pY + 20, 200, 20).build();
+
+        this.addRenderableWidget(saveButton);
     }
 
     @Override
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-        this.renderBackground(graphics); // Размываем задний фон Майнкрафта
+        // ИСПРАВЛЕНО: Новый безопасный рендеринг затемнения экрана в Forge 47.4.22
+        if (this.minecraft != null && this.minecraft.level != null) {
+            graphics.fillGradient(0, 0, this.width, this.height, 0xD0101010, 0xD0101010);
+        }
         
         int pX = this.width / 2;
         int pY = this.height / 2;
 
-        // Рисуем поля на экране
-        this.nameField.render(graphics, mouseX, mouseY, partialTick);
-        this.xField.render(graphics, mouseX, mouseY, partialTick);
-        this.zField.render(graphics, mouseX, mouseY, partialTick);
-        this.radiusField.render(graphics, mouseX, mouseY, partialTick);
+        if (this.nameField != null) this.nameField.render(graphics, mouseX, mouseY, partialTick);
+        if (this.xField != null) this.xField.render(graphics, mouseX, mouseY, partialTick);
+        if (this.zField != null) this.zField.render(graphics, mouseX, mouseY, partialTick);
+        if (this.radiusField != null) this.radiusField.render(graphics, mouseX, mouseY, partialTick);
 
-        // Текстовые подсказки над полями
         graphics.drawCenteredString(this.font, "НАСТРОЙКА СПУТНИКОВОЙ СЕТИ", pX, pY - 80, 0xFFFFFF);
         
         super.render(graphics, mouseX, mouseY, partialTick);
@@ -77,6 +79,6 @@ public class TelemetryConfigScreen extends Screen {
 
     @Override
     public boolean isPauseScreen() {
-        return false; // Игра не будет вставать на паузу в одиночном мире при открытии меню
+        return false; 
     }
 }

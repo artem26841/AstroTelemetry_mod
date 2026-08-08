7df3e10d1072e99409ffdbf7cd24817387ce74d3
artem26841 <artem.andrey.bulat@gmail.com>
2026-08-08 19:44:36 +0300

    TelemetryConfigScreen.java

--- src/main/java/com/science/astrotelemetry/TelemetryConfigScreen.java
+++ src/main/java/com/science/astrotelemetry/TelemetryConfigScreen.java
@@ -10,6 +10,8 @@ public class TelemetryConfigScreen extends Screen {
     private EditBox xField;
     private EditBox zField;
     private EditBox heightField;
+    private Button radiusButton;
+    private double currentRadius;
 
     public TelemetryConfigScreen() {
         super(Component.literal("Управление Исследовательской Станцией"));
@@ -22,46 +24,61 @@ public class TelemetryConfigScreen extends Screen {
         int pX = this.width / 2;
         int pY = this.height / 2;
 
-        // Берем нашу единственную главную зону из менеджера
+        // Получаем нашу главную рабочую зону
         TelemetryZone mainZone = ZoneManager.getZones().get(0);
+        this.currentRadius = mainZone.getRadius();
 
-        this.xField = new EditBox(this.font, pX - 100, pY - 50, 95, 20, Component.literal("X"));
+        // 1. Поля координат X и Z (опускаем чуть ниже, чтобы разнести с заголовком)
+        this.xField = new EditBox(this.font, pX - 100, pY - 45, 95, 20, Component.literal("X"));
         this.xField.setValue(String.valueOf((int)mainZone.getX()));
         this.addWidget(this.xField);
 
-        this.zField = new EditBox(this.font, pX + 5, pY - 50, 95, 20, Component.literal("Z"));
+        this.zField = new EditBox(this.font, pX + 5, pY - 45, 95, 20, Component.literal("Z"));
         this.zField.setValue(String.valueOf((int)mainZone.getZ()));
         this.addWidget(this.zField);
 
-        this.heightField = new EditBox(this.font, pX - 100, pY - 20, 200, 20, Component.literal("Мин. Высота"));
+        // 2. Поле минимальной высоты Y (сдвигаем ниже, убирая наложение)
+        this.heightField = new EditBox(this.font, pX - 100, pY - 5, 200, 20, Component.literal("Мин. Высота"));
         this.heightField.setValue(String.valueOf((int)mainZone.getMinY()));
         this.addWidget(this.heightField);
 
-        // ИСПРАВЛЕНО: Кнопка автоматического перемещения зоны под текущую позицию игрока
+        // 3. НОВОЕ: Кнопка переключения радиуса (области действия)
+        this.radiusButton = Button.builder(Component.literal("Область приема: " + (int)this.currentRadius + " бл."), (button) -> {
+            if (this.currentRadius == 6) this.currentRadius = 12;
+            else if (this.currentRadius == 12) this.currentRadius = 24;
+            else if (this.currentRadius == 24) this.currentRadius = 50;
+            else this.currentRadius = 6;
+            
+            button.setMessage(Component.literal("Область приема: " + (int)this.currentRadius + " бл."));
+        }).bounds(pX - 100, pY + 20, 200, 20).build();
+        this.addRenderableWidget(this.radiusButton);
+
+        // 4. Кнопка GPS привязки
         Button gpsButton = Button.builder(Component.literal("Привязать к моему GPS"), (button) -> {
             this.xField.setValue(String.valueOf((int)this.minecraft.player.getX()));
             this.zField.setValue(String.valueOf((int)this.minecraft.player.getZ()));
             this.heightField.setValue(String.valueOf((int)this.minecraft.player.getY()));
-        }).bounds(pX - 100, pY + 10, 200, 20).build();
+        }).bounds(pX - 100, pY + 50, 200, 20).build();
 
-        // ИСПРАВЛЕНО: Кнопка сохранения изменений существующей зоны
+        // 5. Кнопка сохранения данных
         Button saveButton = Button.builder(Component.literal("Применить координаты"), (button) -> {
             try {
                 double x = Double.parseDouble(this.xField.getValue());
                 double z = Double.parseDouble(this.zField.getValue());
                 double minY = Double.parseDouble(this.heightField.getValue());
 
-                // Перетаскиваем нашу станцию на новые координаты и пишем в JSON
+                // Перезаписываем все параметры в конфиг
                 mainZone.setX(x);
                 mainZone.setZ(z);
                 mainZone.setMinY(minY);
+                mainZone.setRadius(this.currentRadius); // Сохраняем выбранную область покрытия
                 ZoneManager.saveZones();
                 
                 this.onClose(); 
             } catch (NumberFormatException e) {
                 this.heightField.setValue("ОШИБКА ДАННЫХ!");
             }
-        }).bounds(pX - 100, pY + 35, 200, 20).build();
+        }).bounds(pX - 100, pY + 75, 200, 20).build();
 
         this.addRenderableWidget(gpsButton);
         this.addRenderableWidget(saveButton);
@@ -80,15 +97,15 @@ public class TelemetryConfigScreen extends Screen {
         if (this.zField != null) this.zField.renderWidget(graphics, mouseX, mouseY, partialTick);
         if (this.heightField != null) this.heightField.renderWidget(graphics, mouseX, mouseY, partialTick);
 
+        // Корректируем позиции текстов-подсказок (разносим их по высоте)
         String titleText = "КООРДИНАТОР ОУС-1 \"ОБСЕРВАТОРИЯ\"";
-        int textWidth = this.font.width(titleText);
-        graphics.drawString(this.font, titleText, pX - (textWidth / 2), pY - 75, 0xFFFFFF, false);
+        graphics.drawString(this.font, titleText, pX - (this.font.width(titleText) / 2), pY - 75, 0xFFFFFF, false);
         
-        String label1 = "Координаты центра (X / Z):";
-        graphics.drawString(this.font, label1, pX - 100, pY - 62, 0xAAAAAA, false);
+        String label1 = "Координаты центра структуры (X / Z):";
+        graphics.drawString(this.font, label1, pX - 100, pY - 58, 0xAAAAAA, false);
         
-        String label2 = "Минимальная высота приема (Y):";
-        graphics.drawString(this.font, label2, pX - 100, pY - 32, 0xAAAAAA, false);
+        String label2 = "Минимальная высота приема сигнала (Y):";
+        graphics.drawString(this.font, label2, pX - 100, pY - 18, 0xAAAAAA, false);
 
         super.render(graphics, mouseX, mouseY, partialTick);
     }

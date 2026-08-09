98aea1954c5b30e2056abd0932403a3d26e893e3
artem26841 <artem.andrey.bulat@gmail.com>
2026-08-09 08:58:26 +0300

    Update AstroTelemetry.java

--- src/main/java/com/science/astrotelemetry/AstroTelemetry.java
+++ src/main/java/com/science/astrotelemetry/AstroTelemetry.java
@@ -5,7 +5,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.loading.FMLEnvironment;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 

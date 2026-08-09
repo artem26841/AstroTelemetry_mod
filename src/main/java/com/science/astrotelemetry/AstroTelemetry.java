1173ef3677c69e4d94d5450c5bb269553fd76199
artem26841 <artem.andrey.bulat@gmail.com>
2026-08-09 08:47:15 +0300

    AstroTelemetry.java

--- src/main/java/com/science/astrotelemetry/AstroTelemetry.java
+++ src/main/java/com/science/astrotelemetry/AstroTelemetry.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,20 +15,20 @@ public class AstroTelemetry {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public AstroTelemetry() {
-        var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
-        // Регистрируем клиентскую инициализацию (Шаг 3)
-        modEventBus.addListener(this::doClientSetup);
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+            
+            modEventBus.addListener(KeyBindings::registerKeys);
+            modEventBus.addListener(this::doClientSetup);
 
-        // Регистрируем главный автобус событий Forge
-        MinecraftForge.EVENT_BUS.register(this);
-        
-        LOGGER.info("AstroTelemetry успешно инициализирован на стороне клиента!");
+            MinecraftForge.EVENT_BUS.addListener(TelemetryHud::onRenderGui);
+            MinecraftForge.EVENT_BUS.addListener(KeyBindings::onClientTick);
+        }
+        LOGGER.info("!!! AstroTelemetry УСПЕШНО ЗАПУЩЕН !!!");
     }
 
     private void doClientSetup(final FMLClientSetupEvent event) {
-        // Запускаем наш менеджер зон, чтобы он создал или прочитал JSON файл
         event.enqueueWork(ZoneManager::init);
-        LOGGER.info("AstroTelemetry: База данных научных зон загружена.");
+        LOGGER.info("!!! AstroTelemetry: БАЗА ДАННЫХ ИНИЦИАЛИЗИРОВАНА !!!");
     }
 }

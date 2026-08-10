5648702a489817ab5acf00c128acc7648b6593f9
artem26841 <artem.andrey.bulat@gmail.com>
2026-08-10 09:53:37 +0300

    TelemetryConfigScreen.java

--- src/main/java/com/science/astrotelemetry/TelemetryConfigScreen.java
+++ src/main/java/com/science/astrotelemetry/TelemetryConfigScreen.java
@@ -7,20 +7,20 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class TelemetryConfigScreen extends Screen {
-    private final Screen parentScreen; // Переменная для запоминания предыдущего экрана хаба
+    private final Screen parentScreen;
+    private final int zoneIndex; // ИСПРАВЛЕНО: Запоминаем, какую зону настраиваем (0 или 1)
     private EditBox xField;
     private EditBox yField;
     private EditBox zField;
     private EditBox maxHeightField;
     private EditBox radiusField;
 
-      private final int zoneIndex;
     public TelemetryConfigScreen(Screen parentScreen, int zoneIndex) {
         super(Component.literal("Управление Исследовательской Станцией"));
         this.parentScreen = parentScreen;
         this.zoneIndex = zoneIndex;
     }
-    // В методах init() и render() замените .get(0) на .get(this.zoneIndex)
+
     @Override
     protected void init() {
         if (this.minecraft == null || this.minecraft.player == null) return;
@@ -29,30 +29,31 @@ public class TelemetryConfigScreen extends Screen {
         int pX = this.width / 2;
         int pY = this.height / 2;
 
-        TelemetryZone mainZone = ZoneManager.getZones().get(0);
+        // ИСПРАВЛЕНО: Считываем зону по её уникальному индексу из хаба
+        TelemetryZone currentZone = ZoneManager.getZones().get(this.zoneIndex);
 
         this.xField = new EditBox(this.font, pX - 105, pY - 50, 65, 20, Component.literal("X"));
-        this.xField.setValue(String.valueOf((int)mainZone.getX()));
+        this.xField.setValue(String.valueOf((int)currentZone.getX()));
         this.xField.setEditable(isOwner);
         this.addWidget(this.xField);
 
         this.yField = new EditBox(this.font, pX - 35, pY - 50, 65, 20, Component.literal("Y"));
-        this.yField.setValue(String.valueOf((int)mainZone.getY()));
+        this.yField.setValue(String.valueOf((int)currentZone.getY()));
         this.yField.setEditable(isOwner);
         this.addWidget(this.yField);
 
         this.zField = new EditBox(this.font, pX + 35, pY - 50, 65, 20, Component.literal("Z"));
-        this.zField.setValue(String.valueOf((int)mainZone.getZ()));
+        this.zField.setValue(String.valueOf((int)currentZone.getZ()));
         this.zField.setEditable(isOwner);
         this.addWidget(this.zField);
 
         this.maxHeightField = new EditBox(this.font, pX - 105, pY - 10, 100, 20, Component.literal("Макс. Высота"));
-        this.maxHeightField.setValue(String.valueOf((int)mainZone.getMaxHeight()));
+        this.maxHeightField.setValue(String.valueOf((int)currentZone.getMaxHeight()));
         this.maxHeightField.setEditable(isOwner);
         this.addWidget(this.maxHeightField);
 
         this.radiusField = new EditBox(this.font, pX + 5, pY - 10, 100, 20, Component.literal("Радиус области"));
-        this.radiusField.setValue(String.valueOf((int)mainZone.getRadius()));
+        this.radiusField.setValue(String.valueOf((int)currentZone.getRadius()));
         this.radiusField.setEditable(isOwner);
         this.addWidget(this.radiusField);
 
@@ -64,22 +65,22 @@ public class TelemetryConfigScreen extends Screen {
 
         Button saveButton = Button.builder(Component.literal("Применить координаты"), (button) -> {
             try {
-                mainZone.setX(Double.parseDouble(this.xField.getValue()));
-                mainZone.setY(Double.parseDouble(this.yField.getValue()));
-                mainZone.setZ(Double.parseDouble(this.zField.getValue()));
-                mainZone.setMaxHeight(Double.parseDouble(this.maxHeightField.getValue()));
-                mainZone.setRadius(Double.parseDouble(this.radiusField.getValue()));
+                // ИСПРАВЛЕНО: Сохраняем данные именно в ту зону, которую редактировали
+                currentZone.setX(Double.parseDouble(this.xField.getValue()));
+                currentZone.setY(Double.parseDouble(this.yField.getValue()));
+                currentZone.setZ(Double.parseDouble(this.zField.getValue()));
+                currentZone.setMaxHeight(Double.parseDouble(this.maxHeightField.getValue()));
+                currentZone.setRadius(Double.parseDouble(this.radiusField.getValue()));
                 
                 ZoneManager.saveZones();
-                this.minecraft.setScreen(this.parentScreen); // Возвращаемся в хаб после сохранения
+                this.minecraft.setScreen(this.parentScreen); 
             } catch (NumberFormatException e) {
                 this.xField.setValue("ОШИБКА!");
             }
         }).bounds(pX - 105, pY + 45, 210, 20).build();
 
-        // ОБЯЗАТЕЛЬНАЯ КНОПКА НАЗАД ДЛЯ ВОЗВРАТА В ХАБ ПУЛЬТА УПРАВЛЕНИЯ
         Button backButton = Button.builder(Component.literal("Назад"), (button) -> {
-            this.minecraft.setScreen(this.parentScreen); // Просто возвращаемся в хаб без сохранения изменений
+            this.minecraft.setScreen(this.parentScreen); 
         }).bounds(pX - 105, pY + 70, 210, 20).build();
 
         gpsButton.active = isOwner;
@@ -104,12 +105,14 @@ public class TelemetryConfigScreen extends Screen {
         if (this.maxHeightField != null) this.maxHeightField.renderWidget(graphics, mouseX, mouseY, partialTick);
         if (this.radiusField != null) this.radiusField.renderWidget(graphics, mouseX, mouseY, partialTick);
 
-        String titleText = "НАСТРОЙКИ СТАНЦИИ: \"ГЛ КОМП\"";
+        // ИСПРАВЛЕНО: Динамический заголовок в зависимости от выбранной кнопки хаба
+        String zoneName = this.zoneIndex == 0 ? "ЦВМ ОУС-1 \"ГЛ КОМП\"" : "КОМПЛЕКС \"УПОИР v-1.2.1\"";
+        String titleText = "НАСТРОЙКИ СТАНЦИИ: " + zoneName;
         graphics.drawString(this.font, titleText, pX - (this.font.width(titleText) / 2), pY - 75, 0xFFFFFF, false);
         
         graphics.drawString(this.font, "Центр (X / Y / Z):", pX - 105, pY - 62, 0xAAAAAA, false);
         graphics.drawString(this.font, "Макс. высота:", pX - 105, pY - 22, 0xAAAAAA, false);
-        graphics.drawString(this.font, "Радиус обл.:", pX + 5, pY - 22, 0xAAAAAA, false);
+        graphics.drawString(font, "Радиус обл.:", pX + 5, pY - 22, 0xAAAAAA, false);
 
         super.render(graphics, mouseX, mouseY, partialTick);
     }
